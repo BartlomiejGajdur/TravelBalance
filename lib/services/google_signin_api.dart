810bdc24f1fd71d3a2433d806f90dfbfc5203756
BartekG <gajdurbartlomiej@gmail.com>
2024-08-14 12:49:10 +0200

    Update

--- lib/services/google_signin_api.dart
+++ lib/services/google_signin_api.dart
@@ -1,8 +1,14 @@
+import 'dart:io';
+
 import 'package:flutter/material.dart';
 import 'package:google_sign_in/google_sign_in.dart';
 
 class GoogleSignInApi {
-  static final _googleSignIn = GoogleSignIn();
+  static final _googleSignIn = GoogleSignIn(
+    clientId: Platform.isIOS
+        ? '1007518232866-aqth9ace072tfoi7smssfmm9vfkapa8k.apps.googleusercontent.com'
+        : null,
+  );
 
   static Future<GoogleSignInAccount?> login(BuildContext context) async {
     try {
