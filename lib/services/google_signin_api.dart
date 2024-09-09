f3fb468b13c2a9929f282bf45708e5059edefaf8
BartekG <gajdurbartlomiej@gmail.com>
2024-09-09 15:41:06 +0200

    Google sign in FIX IOS

--- lib/services/google_signin_api.dart
+++ lib/services/google_signin_api.dart
@@ -7,7 +7,7 @@ import 'package:google_sign_in/google_sign_in.dart';
 class GoogleSignInApi {
   static final _googleSignIn = GoogleSignIn(
     clientId: Platform.isIOS
-        ? '1007518232866-aqth9ace072tfoi7smssfmm9vfkapa8k.apps.googleusercontent.com'
+        ? '1007518232866-9ef0serrldjq7ti29b7apbhtoqp6tbb6.apps.googleusercontent.com'
         : null,
   );
 
