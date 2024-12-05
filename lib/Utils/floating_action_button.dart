973ce95be4e22c7f22dc8fa440a26bf5cc8c434e
Bartlomiej Gajdur <gajdurbartlomiej@gmail.com>
2024-12-05 14:22:08 +0100

    Upd overlaping + banner

--- lib/Utils/floating_action_button.dart
+++ lib/Utils/floating_action_button.dart
@@ -2,10 +2,9 @@ import 'package:TravelBalance/Utils/globals.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_screenutil/flutter_screenutil.dart';
 
-
 Widget buildFloatingActionButton(BuildContext context, Function() func) {
   return Padding(
-    padding: EdgeInsets.only(bottom: 30.0.h),
+    padding: EdgeInsets.only(bottom: 42.0.h),
     child: FloatingActionButton(
       onPressed: func,
       backgroundColor: secondaryColor,
